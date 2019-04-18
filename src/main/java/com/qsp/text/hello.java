45177fe14cc520626945314abd99c16c67974c6f
qsp533525 <qsp533525@163.com>
2019-04-18 21:38:45 +0800

    秦少沛第一次提交项目

---
+++ src/main/java/com/qsp/text/hello.java
@@ -0,0 +1,12 @@
+package com.qsp.text;
+
+/**
+ * fileName:hello
+ * description:
+ * createTime:2019/4/18 12:25
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
